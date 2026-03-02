56a1ac7025aef08fbd9a28d0d259aae21d28e859
Anthony Belhadj <abelhadj@hpe.com>
2026-03-02 10:10:11 +0100

    Reorders plugin navigation menu items

--- netbox_ddns/navigation.py
+++ netbox_ddns/navigation.py
@@ -1,62 +1,66 @@
 from netbox.plugins import PluginMenuItem, PluginMenuButton, PluginMenu
 
 menu = PluginMenu(
-    label='DDNS',
+    label="DDNS",
     groups=(
-        ('Configuration', (
-            PluginMenuItem(
-                link='plugins:netbox_ddns:managed_dns_name_list',
-                link_text='DNS Names',
-            ),
-            PluginMenuItem(
-                link='plugins:netbox_ddns:server_list',
-                link_text='DDNS Servers',
-                buttons=[
-                    PluginMenuButton(
-                        link='plugins:netbox_ddns:server_add',
-                        title='Add',
-                        icon_class='mdi mdi-plus-thick',
-                    )
-                ]
-            ),
-            PluginMenuItem(
-                link='plugins:netbox_ddns:zone_list',
-                link_text='Forward Zones',
-                buttons=[
-                    PluginMenuButton(
-                        link='plugins:netbox_ddns:zone_add',
-                        title='Add',
-                        icon_class='mdi mdi-plus-thick',
-                    )
-                ]
-            ),
-            PluginMenuItem(
-                link='plugins:netbox_ddns:reversezone_list',
-                link_text='Reverse Zones',
-                buttons=[
-                    PluginMenuButton(
-                        link='plugins:netbox_ddns:reversezone_add',
-                        title='Add',
-                        icon_class='mdi mdi-plus-thick',
-                    )
-                ]
-            ),
-        ),
-         ),
-        ('Extra DNS Names', (
-            PluginMenuItem(
-                link='plugins:netbox_ddns:extradnsname_list',
-                link_text='Extra DNS names',
-                buttons=[
-                    PluginMenuButton(
-                        link='plugins:netbox_ddns:extradnsname_add',
-                        title='Add',
-                        icon_class='mdi mdi-plus-thick',
-                    )
-                ]
+        (
+            "Configuration",
+            (
+                PluginMenuItem(
+                    link="plugins:netbox_ddns:server_list",
+                    link_text="DDNS Servers",
+                    buttons=[
+                        PluginMenuButton(
+                            link="plugins:netbox_ddns:server_add",
+                            title="Add",
+                            icon_class="mdi mdi-plus-thick",
+                        )
+                    ],
+                ),
+                PluginMenuItem(
+                    link="plugins:netbox_ddns:zone_list",
+                    link_text="Forward Zones",
+                    buttons=[
+                        PluginMenuButton(
+                            link="plugins:netbox_ddns:zone_add",
+                            title="Add",
+                            icon_class="mdi mdi-plus-thick",
+                        )
+                    ],
+                ),
+                PluginMenuItem(
+                    link="plugins:netbox_ddns:reversezone_list",
+                    link_text="Reverse Zones",
+                    buttons=[
+                        PluginMenuButton(
+                            link="plugins:netbox_ddns:reversezone_add",
+                            title="Add",
+                            icon_class="mdi mdi-plus-thick",
+                        )
+                    ],
+                ),
+                PluginMenuItem(
+                    link="plugins:netbox_ddns:managed_dns_name_list",
+                    link_text="DNS Names",
+                ),
             ),
         ),
-         ),
+        (
+            "Extra DNS Names",
+            (
+                PluginMenuItem(
+                    link="plugins:netbox_ddns:extradnsname_list",
+                    link_text="Extra DNS names",
+                    buttons=[
+                        PluginMenuButton(
+                            link="plugins:netbox_ddns:extradnsname_add",
+                            title="Add",
+                            icon_class="mdi mdi-plus-thick",
+                        )
+                    ],
+                ),
+            ),
+        ),
     ),
-    icon_class='mdi mdi-router'
+    icon_class="mdi mdi-router",
 )
